3816494b50226fc4e750110de58b289cc5bca273
Patrick-gif955 <patricknewton026@gmail.com>
2025-10-17 14:48:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayToggle() {
+  const menu = document.querySelector(".menu");
+  menu.style.display = "flex";
+}
+
+function hideToggle() {
+  const menu = document.querySelector(".menu");
+  menu.style.display = "none";
+}
